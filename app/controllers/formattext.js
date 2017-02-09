5a4d37133d3bd9e04598ae03357ea74aa2767523
jonfoo <wjianfa@foxmail.com>
2017-02-09 23:36:13 +0800

    new

---
+++ app/controllers/formattext.js
@@ -0,0 +1,94 @@
+function valiJson(jsonString) {
+    try {
+        JSON.parse(jsonString);
+        return true;
+    }
+    catch (err) {
+        // try to throw a more detailed error message using validate
+        //  validateJson(jsonString);
+
+        // rethrow the original error
+
+    }
+
+    return false;
+};
+function looks_like_html(source) {
+    // <foo> - looks like html
+    // <!--\nalert('foo!');\n--> - doesn't look like html
+
+    var trimmed = source.replace(/^[ \t\n\r]+/, '');
+    var comment_mark = '<' + '!-' + '-';
+    return (trimmed && (trimmed.substring(0, 1) === '<' && trimmed.substring(0, 4) !== comment_mark));
+}
+
+app.controller('FormatTextCtrl', ['$scope', '$http', '$timeout', '$sce', '$location', function ($scope, $http, $timeout, $sce, $location) {
+    $scope.isVisible = false;
+    $scope.class = "col-md-12";
+
+    $timeout(function () {
+        var strs = $location.absUrl().split("/"); //字符分割
+        var currentpath = strs[strs.length - 1];
+        var elem = document.getElementById(currentpath);
+        if (elem) {
+            elem.className = "active";
+            $scope.formatbk = currentpath;
+        }
+        else {
+            elem = document.getElementById("json_format");
+            if (elem) {
+                elem.className = "active";
+            }
+            $scope.formatbk = "json_format";
+        }
+    }, 2);
+
+    $scope.formatfc = function () {
+
+        $scope.isVisible = true;
+        $scope.class = "col-md-6 col-sm-6";
+        $scope.destext = "格式化中...";
+
+        if (1 || valiJson($scope.orgtext)) {
+            var default_opts = {
+                indent_size: 4,
+                indent_char: ' ',
+                preserve_newlines: true,
+                jslint_happy: false,
+                keep_array_indentation: false,
+                brace_style: 'collapse',
+                space_before_conditional: true,
+                break_chained_methods: false,
+                selector_separator: '\n',
+                end_with_newline: false
+            };
+            var opts = JSON.parse(JSON.stringify(default_opts));
+            var beautifycode = $scope.orgtext;
+            var result = null;
+            if (looks_like_html(beautifycode)) {
+                beautifycode = html_beautify(beautifycode, opts);
+                result = hljs.highlightAuto(beautifycode, ['xml']);
+            }
+            else {
+                beautifycode = js_beautify(beautifycode, opts);
+                result = hljs.highlightAuto(beautifycode, ['json', 'javascript']);
+            }
+
+            var replacestr = result.value.replace(/\r/g, '·')
+                .replace(/\n/g, '<br>');
+            $scope.myHTML = $sce.trustAsHtml(replacestr);
+        }
+        else {
+            $http.post('/vidadateJson', {'date': $scope.orgtext})
+                .success(function (data, status, headers, config) {
+                    $scope.destext = data;
+                })
+                .error(function () {
+                    $scope.destext = "检查远程调用失败";
+                });
+        }
+
+
+    };
+}]);
+
---
+++ index.js
@@ -0,0 +1,77 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var log4js = require('log4js');
+
+log4js.loadAppender('file');
+//log4js.addAppender(log4js.appenders.console());
+log4js.addAppender(log4js.appenders.file('logs/visit.log'), 'visit');
+
+
+var app = module.exports = express();
+
+// Register ejs as .html. If we did
+// not call this, we would need to
+// name our views foo.ejs instead
+// of foo.html. The __express method
+// is simply a function that engines
+// use to hook into the Express view
+// system by default, so if we want
+// to change "foo.ejs" to "foo.html"
+// we simply pass _any_ function, in this
+// case `ejs.__express`.
+
+app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/app'));
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set('view engine', 'html');
+
+
+var des = {
+  json_format: {des: "json在线解析 在线工具 格式化 高亮显示 缩进 全屏 简洁 智能 自动 万能", title: "json在线解析 格式化", name: "json解析格式化"},
+  js_format: {
+    des: " javascript在线解析 在线工具 格式化 高亮显示 缩进 全屏 简洁 智能 自动 万能",
+    title: "javascript 在线解析 格式化",
+    name: "javascript(js)解析格式化"
+  },
+  xml_format: {des: "xml在线解析 在线工具 格式化 高亮显示 缩进 全屏 简洁 智能 自动 万能", title: "xml  在线解析 格式化", name: "xml解析格式化"},
+  html_format: {des: "html在线解析 在线工具 格式化 高亮显示 缩进 全屏 简洁 智能 自动 万能", title: "html  在线解析 格式化", name: "html解析格式化"},
+  auto_format: {des: "万能代码在线格式化  在线解析 在线工具 格式化 高亮显示 缩进 全屏 简洁 智能 自动 万能", title: "万能代码 在线格式化", name: "万能解析格式化"}
+};
+app.get('/', function(req, res){
+  res.redirect('/json_format');
+});
+
+app.get('/:page_type', function(req, res){
+  var logger = log4js.getLogger('visit');
+  logger.setLevel('DEBUG');
+  logger.debug(req.hostname + "/" + req.params.page_type + " <<------- " + req.ip);
+
+  if(!des[req.params.page_type])
+  {
+    res.status(404);
+    res.send("not found!!!");
+    return;
+  }
+  res.render('app', {
+    title: "EJS example",
+    header: "Some users",
+    alltype: des,
+    pagetype: req.params.page_type
+  });
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(80);
+  console.log('Express started on port 80');
+}
